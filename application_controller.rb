6105c66f7a12fcb1755164d590a2c182ac8aec60
Arieta Haskaj <arietah8545@hstat.org>
2019-03-01 22:34:47 +0000

    mood detection wors on back end and hide keys

--- application_controller.rb
+++ application_controller.rb
@@ -1,13 +1,8 @@
 require 'dotenv/load'
 require 'bundler'
 Bundler.require
-# Content-Type: application/json
-# Host: api.twinword.com
-# X-Twaip-Key: ENV["TWAIP-KEY"]
 
 require_relative 'models/model.rb'
-
-# http://www.omdbapi.com
 
 class ApplicationController < Sinatra::Base
 
@@ -19,7 +14,6 @@ class ApplicationController < Sinatra::Base
     puts params
     @user_mood = params[:mood]
     puts @user_mood
-    
     erb :result
   end
 end
--- models/model.rb
+++ models/model.rb
@@ -1,3 +1,8 @@
+require 'paralleldots'
+# I had to put 'gem install paralleldots' into the terminal,
+# so can everyone access the gem? What about when we deploy the app?
+# Is is only running locally?
+
 moods = {
     :happy => ["Comedy", "Adventure", "Action"],
     :sad => ["Drama", "Romantic Comedy"],
@@ -5,3 +10,12 @@ moods = {
     :love => ["Romance, Romantic Comedy"],
     :fear => ["Horror"]
 }
+
+set_api_key(ENV["PARALLEL_API"])
+# get_api_key
+
+def get_mood(mood)
+    @user_mood=emotion(mood)
+    @user_mood["emotion"]["emotion"]
+end
+# puts get_mood("happy")
