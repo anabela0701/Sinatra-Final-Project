1c71e00292ac1b01a776498120a471f3996bb51a
raquelj6437 <raquelj6437@hstat.org>
2019-03-12 12:25:45 -0400

    Update model.rb

--- models/model.rb
+++ models/model.rb
@@ -1,4 +1,3 @@
-require 'omdb/api'
 require 'paralleldots'
 require 'dotenv/load'
 require 'pp'
@@ -16,56 +15,19 @@ $moods = {
     "fear" => ["Horror", "Comedy"],
     "bored" => ["Comedy", "Action", "Thriller"]
 }
-
-used_movie_id = []
-movie_list = []
-
-# nine character in id
-# all start with tt
-
-def find_genre
-    moods.each do |mood,genres|
-        if @user_mood == mood
-            return genres
-        end
-    end
-end
-
-def make_id
-	movie_id = []
-	movie_id.push("tt")
-	if movie_id.length <= 7
-		num = rand(9)
-		movie_id.push
-	else
-		movie_id = movie_id.join
-		check_used_id(movie_id)
-	end
-end
-
-def check_used_id(id)
-    genres = find_genre
-	used_movie_id.each do |i|
-		if i == id
-		    movie_id
-	    else
-	        movie = client.find_by_id(id)
-	        genres.each do |genre|
-	            movie.each do |movie_genre|
-	                if genre == movie_genre
-	                    movie_list.push(id)
-	                end
-	            end
-	        end
-        end
-	end
-end
-
 # @user_mood = 'happy'
 
-client = Omdb::Api::Client.new(api_key: ENV["MOVIE_API"])
+require 'json'
+require 'open-uri'
 
-movie = client.find_by_id('tt0083929')
+id = 'tt2294629'
+json = JSON.parse(open("http://www.omdbapi.com/?i=" + id + "&apikey=d3d6e011"){ |x| x.read })
+
+puts json
+
+json.each do |x,i|
+    puts i
+end
 
 # puts movie
 # pp client.search(genre = 'comedy').inspect.split('#')
